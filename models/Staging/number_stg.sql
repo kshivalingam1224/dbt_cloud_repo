c7552b1c4d29cd36f9e69a2607d9e523b98cb176
Shiva K <shivalingamagyp@gmail.com>
2024-05-16 07:49:00 +0000

    sdsds

--- models/Staging/number_stg.sql
+++ models/Staging/number_stg.sql
@@ -1,3 +1,4 @@
 {{ config(schema='numbers_calculation_stg') }}
-select number1,pow(number1,1),pow(number1,2),pow(number1,3),pow(number1,4),pow(number1,5) as mul 
+select number1,pow(number1,1) as power1,pow(number1,2) as power2,pow(number1,3) as power3,pow(number1,4) as power4
+,pow(number1,5) as power5 
 from shaped-crossbar-422705-f1.numbers_raw.numbers
