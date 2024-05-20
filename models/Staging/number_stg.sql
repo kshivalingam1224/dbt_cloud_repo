c432ed31fb91270e7b94112f80f1b7c4042b9ed8
Shiva K <shivalingamagyp@gmail.com>
2024-05-20 12:23:32 +0000

    ddd

--- models/Staging/number_stg.sql
+++ models/Staging/number_stg.sql
@@ -1,4 +1,3 @@
-{{ config(schema='numbers_calculation_stg') }}
 select number1,pow(number1,1) as power1,pow(number1,2) as power2,pow(number1,3) as power3,pow(number1,4) as power4
 ,pow(number1,5) as power5 
 from {{ source('numbers_raw_alias', 'numbers_alias') }}
