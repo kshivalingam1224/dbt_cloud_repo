6905a590ac48ce5a8973594575959c8226551b7f
Shiva K <shivalingamagyp@gmail.com>
2024-07-16 05:44:30 +0000

    sss

--- models/Staging/number_stg.sql
+++
@@ -1,3 +0,0 @@
-select number1,pow(number1,1) as power1,pow(number1,2) as power2,pow(number1,3) as power3,pow(number1,4) as power4
-,pow(number1,5) as power5 
-from {{ source('numbers_raw_alias', 'numbers_alias') }}
